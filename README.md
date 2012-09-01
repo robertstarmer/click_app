d3d515b8a92567c799b36c6743fe85baf3c5800d
Robert Starmer <rstarmer@gmail.com>
2012-08-31 23:04:31 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Click App
+=========
+
+A simple app to showcase the nosql and storm pipeline data tools model
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickApp::Application.config.secret_token = '7439967deafa95e3739023a5c58a75f7601a66304f03bd47579e36891815fc0cc0efeb62cdc6b6435edce6a9783b4085ddb9130b7142509942ecb2d4779f3e69'
